f5c2b479fc0d705232836f9c47d8b633d828d94b
Irina Nikolaidi <irina.nikolaidi@tui.ru>
2020-03-15 19:54:11 +0300

    fix names flat promises

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import path from 'path';
 import buildFileName from './utils/buildFileName';
 
 export default (dirPath, href) => {
-  const filename = buildFileName(href);
-  const filepath = path.join(dirPath, filename);
-  return axios.get(href)
-    .then(({ data }) => fs.writeFile(filepath, data)
-      .catch((e) => {
-        throw e;
-      }))
-    .catch((e) => {
-      throw e;
-    });
+	const filename = buildFileName(href);
+	const filepath = path.join(dirPath, filename);
+	return axios
+		.get(href)
+		.then(({ data }) => fs.writeFile(filepath, data))
+		.then(() => {
+			console.log('File was saved!')
+		})
+		.catch((e) => {
+			throw e;
+		})
 };
--- src/utils/buildFileName.js
+++ src/utils/buildFileName.js
@@ -1,8 +1,8 @@
 import url from 'url';
 
 export default (href) => {
-  const { path: urlPath, host } = url.parse(href);
-  const filename = [host, urlPath]
+  const { path, host } = url.parse(href);
+  const filename = [host, path]
     .join('')
     .replace(/[^\w]/g, '_');
   return `${filename}.html`;
