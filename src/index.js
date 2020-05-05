f181f81957ce1e88017cf6a476c88f1f1c43a402
Irina Nikolaidi <irina.nikolaidi@tui.ru>
2020-05-05 20:38:15 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import debug from 'debug';
 import axios from 'axios';
-import { promises as fs } from 'fs';
+import {promises as fs} from 'fs';
 import path from 'path';
 import Listr from 'listr';
 import axiosDebug from 'axios-debug-log';
 import {
-  buildFileName, buildRemoteUrls, getAllLocalResources, replaceWithLocalUrls,
+  buildFileNameFromUrl, buildRemoteUrls, replaceWithLocalUrls,
 } from './utils';
 
-axiosDebug({
+const axiosDebugConfig = {
   request: (log, config) => {
     log(`Request to ${config.url}`);
   },
@@ -21,7 +21,7 @@ axiosDebug({
   error: (log, error) => {
     log('Error', error);
   },
-});
+};
 
 const log = debug('page-loader');
 
@@ -30,39 +30,44 @@ const stateNames = {
   error: 'error',
 };
 
-export default (dirPath, href) => {
+export default (dirPath, url) => {
+  axiosDebug(axiosDebugConfig);
   let data;
-  const { filename, dirname } = buildFileName(href);
-  const htmlFilePath = path.join(dirPath, filename);
-  log(`dirname: ${dirname}`);
-  log(`filename: ${filename}`);
-  const filesDirPath = path.join(dirPath, dirname);
+  const htmlFileName = buildFileNameFromUrl(url, '.html');
+  const filesDirName = buildFileNameFromUrl(url, '_files');
+  const htmlFilePath = path.join(dirPath, htmlFileName);
+  const filesDirPath = path.join(dirPath, filesDirName);
+
+  log(`htmlFileName: ${htmlFileName}`);
+  log(`filesDirName: ${filesDirName}`);
   log(`htmlFilePath: ${htmlFilePath}`);
   log(`filesDirPath: ${filesDirPath}`);
 
   return fs.stat(dirPath)
-    .then(() => axios.get(href))
+    .then(() => axios.get(url))
     .then((response) => {
       data = response.data;
-      const modifiedData = replaceWithLocalUrls(data, dirname, href);
+      const modifiedData = replaceWithLocalUrls(data, filesDirName, url);
       return fs.writeFile(htmlFilePath, modifiedData);
     })
+
     .then(() => {
-      const allLocalResources = getAllLocalResources(data, href);
-      const allRemoteUrls = buildRemoteUrls(allLocalResources, href);
-      log('allRemoteUrls:');
-      log(`${allRemoteUrls.join('\n')}`);
+      const allRemoteUrls = buildRemoteUrls(data, url);
+
+      log(`all remote urls:\n${allRemoteUrls.join('\n')}`);
+
       const innerPromises = allRemoteUrls.map((url) => axios
-        .get(url, { responseType: 'arraybuffer' })
-        .then((value) => ({ state: stateNames.success, payload: value }))
-        .catch((e) => ({ state: stateNames.error, payload: e })));
+        .get(url, {responseType: 'arraybuffer'})
+        .then((value) => ({state: stateNames.success, payload: value}))
+        .catch((e) => ({state: stateNames.error, payload: e})));
       return Promise.all(innerPromises);
     })
+
     .then((responses) => {
       const tasks = new Listr(
         responses.map((response) => {
-          const { url } = response.payload.config;
-          const { pathname } = new URL(url);
+          const {url} = response.payload.config;
+          const {pathname} = new URL(url);
           const filePath = path.join(filesDirPath, pathname);
           if (response.state === stateNames.error) {
             return {
@@ -70,17 +75,17 @@ export default (dirPath, href) => {
               task: () => Promise.reject(new Error(response.payload)),
             };
           }
-          const { dir } = path.parse(filePath);
+          const {dir} = path.parse(filePath);
           return {
             title: url,
-            task: () => fs.mkdir(dir, { recursive: true })
+            task: () => fs.mkdir(dir, {recursive: true})
               .then(() => {
                 log(response.payload.data);
                 return fs.writeFile(filePath, response.payload.data);
               }),
           };
         }),
-        { concurrent: true, exitOnError: false },
+        {concurrent: true, exitOnError: false},
       );
       return tasks.run();
     })
--- src/utils.js
+++ src/utils.js
@@ -1,28 +1,19 @@
 import cheerio from 'cheerio';
-import { flatten } from 'lodash';
-import { parse as parseUrl } from 'url';
+import {parse as parseUrl} from 'url';
 import path from 'path';
 
 export const isUrlLocal = (url, base) => {
-  const { host: baseHost } = new URL(base);
-  const { host } = new URL(url, base);
+  const {host: baseHost} = new URL(base);
+  const {host} = new URL(url, base);
   return host === baseHost;
 };
 
-export const buildFileName = (href) => {
-  const { host, path: hrefPath } = parseUrl(href);
-  const baseName = [host, hrefPath]
+export const buildFileNameFromUrl = (url, postfix) => {
+  const {host, path: urlPath} = parseUrl(url);
+  const baseName = [host, urlPath]
     .join('')
     .replace(/[^\w]/g, '_');
-  const filename = `${baseName}.html`;
-  const dirname = `${baseName}_files`;
-  return { filename, dirname };
-};
-
-export const buildRemoteUrls = (urls, href) => {
-  const { origin } = new URL(href);
-
-  return urls.map((url) => new URL(url, origin).toString());
+  return `${baseName}${postfix}`;
 };
 
 const tags = {
@@ -31,25 +22,27 @@ const tags = {
   script: 'src',
 };
 
-export const getAllLocalResources = (data, link) => {
+export const buildRemoteUrls = (data, link) => {
+  const {origin} = new URL(link);
   const $ = cheerio.load(data);
   const getAllSourcesFromOneTag = (tag) => $(tag).map((i, item) => $(item).attr(tags[tag])).get();
 
   const allLinks = Object.keys(tags).map(getAllSourcesFromOneTag);
-  return flatten(allLinks)
-    .filter((item) => item !== '')
+  return allLinks
+    .flat()
     .filter((item) => isUrlLocal(item, link))
-    .filter((item) => !item.endsWith('/'));
+    .map((url) => new URL(url, origin).toString());
 };
 
 export const replaceWithLocalUrls = (data, dirPath, url) => {
   const $ = cheerio.load(data);
+
   Object.keys(tags).forEach((tag) => {
     $(tag).each((i, item) => {
       const link = $(item).attr(tags[tag]);
       if (link && isUrlLocal(link, url)) {
-        const { origin } = new URL(url);
-        const { pathname } = new URL(link, origin);
+        const {origin} = new URL(url);
+        const {pathname} = new URL(link, origin);
         const newAttr = path.join(dirPath, pathname);
         $(item).attr(tags[tag], newAttr);
       }
