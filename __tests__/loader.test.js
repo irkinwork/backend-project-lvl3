1529345b1fb344a8adef648ddad12f7d3c4aee58
Irina Nikolaidi <irina.nikolaidi@tui.ru>
2020-05-05 18:26:57 +0300

    refactor tests

--- __tests__/loader.test.js
+++ __tests__/loader.test.js
@@ -6,7 +6,7 @@ import loadPage from '../src';
 
 nock.disableNetConnect();
 const getFixturePath = (name) => path.join(__dirname, '..', '__fixtures__', name);
-let dirpath, expected, expectedSource, scope;
+let dirpath, expected, expectedSource;
 
 const link = 'https://frontend-project-lvl3-gamma.now.sh';
 const urlPath = '/';
@@ -16,23 +16,18 @@ const htmlFilenameSource = `${nameBase}__source.html`;
 const dirname = `${nameBase}_files`;
 const getFixtureFilesPath = (name) => path.join(__dirname, '..', '__fixtures__', dirname, name);
 
-beforeEach(async () => {
-  const resourcesPaths = [
-    '/favicon.ico',
-    '/css/bootstrap.min.css',
-    '/main.js',
-  ];
+const resourcesPaths = [
+  '/favicon.ico',
+  '/css/bootstrap.min.css',
+  '/main.js',
+];
+const scope = nock(link).log(console.log);
 
-  scope = nock(link).log(console.log);
+beforeEach(async () => {
+  dirpath = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-rss-'));
   scope
     .get(urlPath)
-    .reply(200, expectedSource)
-  resourcesPaths.forEach((path) => {
-    scope
-      .get(path)
-      .reply(200, fs.readFile(getFixtureFilesPath(path)))
-  });
-  dirpath = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-rss-'));
+    .reply(200, expectedSource);
 });
 
 beforeAll(async () => {
@@ -41,13 +36,25 @@ beforeAll(async () => {
 });
 
 test('page-loader should successfully load the https://frontend-project-lvl3-gamma.now.sh', async () => {
-    await loadPage(dirpath, link);
-    const actualFileName = path.join(dirpath, htmlFilename);
-    const actual = await fs.readFile(actualFileName, 'utf-8');
-    await expect(actual.trim()).toBe(expected.trim());
+  resourcesPaths.forEach((path) => {
+    const resourceData = fs.readFile(getFixtureFilesPath(path));
+    scope
+      .get(path)
+      .reply(200, resourceData)
+  });
+  await loadPage(dirpath, link);
+  const actualFileName = path.join(dirpath, htmlFilename);
+  const actual = await fs.readFile(actualFileName, 'utf-8');
+  await expect(actual.trim()).toBe(expected.trim());
 });
 
-test('page-loader should load https://frontend-project-lvl3-gamma.now.sh with listr error', async () => {
+describe('errors', () => {
+  test('page-loader should load https://frontend-project-lvl3-gamma.now.sh with listr error', async () => {
+    resourcesPaths.forEach((path) => {
+      scope
+        .get(path)
+        .reply(404)
+    });
     try {
       await loadPage(dirpath, link);
     } catch (e) {
@@ -56,33 +63,34 @@ test('page-loader should load https://frontend-project-lvl3-gamma.now.sh with li
       expect(actual.trim()).toBe(expected.trim());
       expect(e.message).toBe('Something went wrong');
     }
-});
+  });
 
-test('page-loader should fail and show ENOENT error', async () => {
-  await expect(loadPage('/wrong/path', link)).rejects.toThrow('ENOENT');
-});
+  test('page-loader should fail and show ENOENT error', async () => {
+    await expect(loadPage('/wrong/path', link)).rejects.toThrow('ENOENT');
+  });
 
-test('page-loader should fail and show EACCES error', async () => {
-  await expect(loadPage('/root', link)).rejects.toThrow('EACCES');
-});
+  test('page-loader should fail and show EACCES error', async () => {
+    await expect(loadPage('/root', link)).rejects.toThrow('EACCES');
+  });
 
-test('page-loader should fail and show 400 error', async () => {
-  const errorUrl = 'https://frontend-project-lvl3-gamma.now.sh/error';
-  const urlPath = '/error';
-  scope
-    .get(urlPath)
-    .reply(404, []);
+  test('page-loader should fail and show 400 error', async () => {
+    const errorUrl = 'https://frontend-project-lvl3-gamma.now.sh/error';
+    const urlPath = '/error';
+    scope
+      .get(urlPath)
+      .reply(404, []);
 
-  await expect(loadPage(dirpath, errorUrl)).rejects.toThrow('404');
-});
+    await expect(loadPage(dirpath, errorUrl)).rejects.toThrow('404');
+  });
 
-test('page-loader should fail and show 500 error', async () => {
-  const errorUrl = 'https://frontend-project-lvl3-gamma.now.sh/error';
-  const urlPath = '/error';
-  scope
-    .get(urlPath)
-    .reply(500, []);
+  test('page-loader should fail and show 500 error', async () => {
+    const errorUrl = 'https://frontend-project-lvl3-gamma.now.sh/error';
+    const urlPath = '/error';
+    scope
+      .get(urlPath)
+      .reply(500, []);
 
-  await expect(loadPage(dirpath, errorUrl)).rejects.toThrow('500');
-});
+    await expect(loadPage(dirpath, errorUrl)).rejects.toThrow('500');
+  });
+})
 
