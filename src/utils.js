c35fe7440450d093b0b7855f5abe8ba52d39a7ab
Irina Nikolaidi <irina.nikolaidi@tui.ru>
2020-05-22 03:18:47 +0300

    fix utils

--- src/utils.js
+++ src/utils.js
@@ -18,11 +18,11 @@ const tags = {
 
 export const prepareData = (data, dirPath, baseUrl) => {
   const $ = cheerio.load(data);
+  const { origin } = new URL(baseUrl);
+  const getNormalized = (link) => new URL(link, baseUrl);
 
-  const getNewLink = (link) => new URL(link, baseUrl);
-
-  const getNormalized = (sourceLink) => {
-    const normalizedUrl = getNewLink(sourceLink);
+  const getHash = (sourceLink) => {
+    const normalizedUrl = getNormalized(sourceLink);
     const normalized = normalizedUrl.toString();
     const relative = path.join(dirPath, normalizedUrl.pathname);
     return { sourceLink, normalized, relative };
@@ -34,8 +34,9 @@ export const prepareData = (data, dirPath, baseUrl) => {
 
   const hash = Object.keys(tags)
     .flatMap(getAllSourcesFromOneTag)
-    .filter((link) => getNewLink(link).pathname !== '/')
-    .map(getNormalized);
+    .filter((link) => getNormalized(link).pathname !== '/')
+    .filter((link) => getNormalized(link).origin === origin)
+    .map(getHash);
 
   const links = hash.map(({ normalized }) => normalized);
   const html = hash
